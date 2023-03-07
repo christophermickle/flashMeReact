7bd4625b04de99feb53381e801bfd864eb80bb15
Christopher Mickle <cd.mickle@yahoo.com>
2023-03-07 12:57:43 -0800

    Updated progress on app refactoring for React, background added, open/close modal added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useRef } from "react";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [deck, setDeck] = useState([]);
+  const [currentCard, setCurrentCard] = useState([]);
+  const showModalHandler = () => {modalRef.current.showModal()};
+  const closeModalHandler = () => {modalRef.current.close()};
+  const modalRef = useRef();
 
   return (
-    <div className="App">
-      <h1>FlashMe</h1>
+    <div className="app">
+      <h1 className='title' contenteditable='true'>
+        {" "}
+        My Deck...
+      </h1>
+      <main className='deckContainer'>
+        <div className='card'>
+          <p id='frontDisplay'></p>
+          <div className='borderBreak'></div>
+          <p id='backDisplay'></p>
         </div>
-  )
+        <div className='buttons'>
+          <button id='showAnswer'></button>
+          <button id='showModal' onClick={showModalHandler}>+</button>
+          <button id='nextCard'></button>
+        </div>
+      </main>
+
+      <dialog ref={modalRef} id='newCardModal'>
+
+        <div className='newCardWrapper'>
+          <button id='closeModal' onClick={closeModalHandler}>X</button>
+          <input
+            id='cardFrontText'
+            name='cardFront'
+            type='text'
+            placeholder='Front of card'
+          />
+          <input
+            id='cardBackText'
+            name='cardBack'
+            type='text'
+            placeholder='Back of card'
+          />
+          <button id='saveButton'>Save card</button>
+        </div>
+      </dialog>
+    </div>
+  );
 }
 
-export default App
+export default App;
